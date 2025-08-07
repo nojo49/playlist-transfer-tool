de254be516a7dacf072c50ac2bc1e4ea9ebb5e6c
Noah Johnson <noahjohnson@Noahs-MacBook-Air.local>
2025-08-06 19:25:33 -0500

    rearrange auth, get playlists, sort out unowned playlists

--- SpotifyClient.py
+++ SpotifyClient.py
@@ -1,16 +1,19 @@
 import requests
 import webbrowser
 import base64
+import json
+
 
 class SpotifyClient:
     CONST_AUTH_URL = 'https://accounts.spotify.com/api/token'
     CONST_USER_AUTH_URL_BASE = 'https://accounts.spotify.com/authorize?'
     CONST_CURRENT_USER_URL = 'https://api.spotify.com/v1/me'
-    
+    CONST_USERS_BASE_URL = 'https://api.spotify.com/v1/users/'
+ 
     def __init__(self, client_id, client_secret):
-       self.client_id = client_id
-       self.client_secret = client_secret
-       self.redirect_uri = 'http://127.0.0.1:3000'
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = 'http://127.0.0.1:3000'
 
     def get_auth_token(self):
         # base64 encode the client id and secret for auth header
@@ -19,23 +22,38 @@ class SpotifyClient:
         b64_bytes = base64.b64encode(auth_string_ascii_bytes)
         b64_encode_auth_string = b64_bytes.decode("ascii")
 
-        response = requests.post(self.CONST_AUTH_URL, {'grant_type': 'authorization_code', 'code': self.user_auth_code, 'redirect_uri': self.redirect_uri},
-                                 headers={'Authorization': 'Basic ' + b64_encode_auth_string, 'Content-Type': 'application/x-www-form-urlencoded'})
+        response = requests.post(self.CONST_AUTH_URL,
+                                 {'grant_type': 'authorization_code',
+                                  'code': self.user_auth_code,
+                                  'redirect_uri': self.redirect_uri},
+                                 headers={'Authorization': 'Basic ' + b64_encode_auth_string,
+                                   'Content-Type': 'application/x-www-form-urlencoded'})
         
         self.auth_token = response.json()['access_token']
         
     def get_current_user_id(self):
-        response = requests.get(self.CONST_CURRENT_USER_URL, headers = {'Authorization': 'Bearer ' + self.auth_token})
+        response = requests.get(self.CONST_CURRENT_USER_URL, headers={'Authorization': 'Bearer ' + self.auth_token})
         self.current_user_id = response.json()['display_name']
         
     def get_user_auth(self):
         # prompt user to permit access / login to account
-        auth_url = f"{self.CONST_USER_AUTH_URL_BASE}client_id={self.client_id}&response_type=code&redirect_uri={self.redirect_uri}&show_dialog=true"
+        auth_url = f"{self.CONST_USER_AUTH_URL_BASE}client_id={self.client_id}&response_type=code&scope=playlist-read-private playlist-read-collaborative&redirect_uri={self.redirect_uri}&show_dialog=true"
         webbrowser.open(auth_url)
         self.user_auth_code = input("Please input the auth code url query param following ?code= \n")
     
-    def get_user_playlists(self):
+    def run_auth_flow(self):
         self.get_user_auth()
         self.get_auth_token()
         self.get_current_user_id()
-        
+   
+    def get_user_playlists(self):
+        playlist_url = self.CONST_USERS_BASE_URL + self.current_user_id + '/playlists'
+        all_playlists_json = requests.get(playlist_url,{'limit': 50},
+                                      headers={
+                                          'Authorization': 'Bearer ' + self.auth_token
+                                      })
+        all_playlists = all_playlists_json.json()
+        for playlist in all_playlists['items']:
+            if playlist['owner']['id'] != self.current_user_id:
+                continue
+            print(playlist['id'] + ' - ' + playlist['owner']['id'])
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from SpotifyClient import SpotifyClient
 
+
 def main():
     input_client_id = input("Spotify client id: \n")
     input_client_secret = input("Spotify client secret: \n")
     sc = SpotifyClient(input_client_id, input_client_secret)
+    sc.run_auth_flow()
     sc.get_user_playlists()
+
 
 if __name__ == "__main__":
     main()
